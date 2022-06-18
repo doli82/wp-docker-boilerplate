84aba3ef94a57ea2687c5cd4c269785fc954af54
Daniel Oliveira <daniel.go@msn.com>
2022-06-18 15:33:40 -0300

    Adicionado plugin de exemplo

---
+++ src/plugins/example-plugin/example.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Plugin de Exemplo
+Plugin URI: https://github.com/doli82/wp-docker-boilerplate
+Description: Used by millions, Akismet is quite possibly the best way in the world to <strong>protect your blog from spam</strong>. It keeps your site protected even while you sleep. To get started: activate the Akismet plugin and then go to your Akismet Settings page to set up your API key.
+Version: 1.0.0
+Author: Daniel Oliveira
+Author URI: https://www.linkedin.com/in/doli/
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+*/
